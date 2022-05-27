31bc94c04d72403ae7f784458ec3db68f66b4b8a
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-05-27 19:13:10 +0700

    sample math test with jest

---
+++ src/math.test.js
@@ -0,0 +1,7 @@
+import { add, multiply, substract } from './math'
+
+it('can do some math', () => {
+    expect(add(4, 2)).toBe(6);
+    expect(multiply(4, 2)).toBe(8);
+    expect(substract(4, 2)).toBe(2);
+})
